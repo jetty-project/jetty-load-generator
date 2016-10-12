a8cb3b97e41151fb1eac0a278c00acd67b6cb30f
olivier lamy <olamy@webtide.com>
2016-10-12 21:19:30 +1100

    add more help for jenkins remote call
    Signed-off-by: olivier lamy <olamy@webtide.com>

--- jetty-load-generator-starter/src/main/java/org/eclipse/jetty/load/generator/starter/JenkinsRemoteStarter.java
+++ jetty-load-generator-starter/src/main/java/org/eclipse/jetty/load/generator/starter/JenkinsRemoteStarter.java
@@ -1,5 +1,8 @@
 package org.eclipse.jetty.load.generator.starter;
 
+import com.beust.jcommander.JCommander;
+import org.eclipse.jetty.load.generator.responsetime.ResponseTimeListener;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FilterInputStream;
@@ -8,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.List;
 
 /**
  * Class to start remote process for Jenkins
@@ -15,7 +19,19 @@ import java.net.Socket;
 public class JenkinsRemoteStarter
 {
 
-    public static void main(String... args)  throws Exception {
+    public static List<ResponseTimeListener> responseTimeListeners;
+
+    public static List<ResponseTimeListener> getResponseTimeListeners()
+    {
+        return responseTimeListeners;
+    }
+
+    public static void setResponseTimeListeners( List<ResponseTimeListener> responseTimeListeners )
+    {
+        JenkinsRemoteStarter.responseTimeListeners = responseTimeListeners;
+    }
+
+    public static void main( String... args)  throws Exception {
         String slaveAgentSocket = args[0];
         int i = slaveAgentSocket.indexOf(':');
         main( slaveAgentSocket.substring(0, i), Integer.parseInt(slaveAgentSocket.substring(i+1) ));
@@ -71,4 +87,54 @@ public class JenkinsRemoteStarter
             out.close();
         }
     }
+
+
+    public static void launch(List<String> argsList) throws Exception {
+
+        final String[] args = argsList.toArray(new String[argsList.size()]);
+
+        LoadGeneratorStarterArgs runnerArgs = new LoadGeneratorStarterArgs();
+
+        try
+        {
+            JCommander jCommander = new JCommander( runnerArgs, args );
+            if ( runnerArgs.isHelp() )
+            {
+                jCommander.usage();
+                System.exit( 0 );
+            }
+        }
+        catch ( Exception e )
+        {
+            new JCommander( runnerArgs ).usage();
+        }
+
+        try
+        {
+            LoadGeneratorStarter runner = new LoadGeneratorStarter( runnerArgs ){
+                @Override
+                protected ResponseTimeListener[] getResponseTimeListeners()
+                {
+                    return responseTimeListeners.toArray(new ResponseTimeListener[responseTimeListeners.size()]);
+                }
+            };
+
+            if (runnerArgs.getRunIteration() > 0)
+            {
+                runner.run(runnerArgs.getRunIteration());
+            } else
+            {
+                runner.run();
+            }
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+            new JCommander( runnerArgs ).usage();
+            System.exit( 1 );
+        }
+
+    }
+
+
 }
