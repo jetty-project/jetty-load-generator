9a05479115c7cef39da422ec3b951ba5342112ea
olivier lamy <olamy@webtide.com>
2017-10-12 21:50:43 +1100

    empty constructor and add license
    Signed-off-by: olivier lamy <olamy@webtide.com>

--- jetty-load-generator-listeners/src/main/java/org/mortbay/jetty/load/generator/listeners/LoadResult.java
+++ jetty-load-generator-listeners/src/main/java/org/mortbay/jetty/load/generator/listeners/LoadResult.java
@@ -1,14 +1,34 @@
-package org.mortbay.jetty.load.generator.listeners;
+//
+//  ========================================================================
+//  Copyright (c) 1995-2017 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
 
-import org.mortbay.jetty.load.generator.listeners.CollectorInformations;
-import org.mortbay.jetty.load.generator.listeners.ServerInfo;
+package org.mortbay.jetty.load.generator.listeners;
 
 public class LoadResult
 {
 
-    private final ServerInfo serverInfo;
+    private ServerInfo serverInfo;
 
-    private final CollectorInformations collectorInformations;
+    private CollectorInformations collectorInformations;
+
+    public LoadResult()
+    {
+        // no op
+    }
 
     public LoadResult( ServerInfo serverInfo, CollectorInformations collectorInformations )
     {
