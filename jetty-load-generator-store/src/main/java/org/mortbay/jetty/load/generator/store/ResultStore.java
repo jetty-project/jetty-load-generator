90bac5edda5dddaf3fb7af6b16b500e91b701e9d
olivier lamy <olamy@webtide.com>
2017-10-19 11:50:14 +1100

    add missing license header
    Signed-off-by: olivier lamy <olamy@webtide.com>

--- jetty-load-generator-store/src/main/java/org/mortbay/jetty/load/generator/store/ResultStore.java
+++ jetty-load-generator-store/src/main/java/org/mortbay/jetty/load/generator/store/ResultStore.java
@@ -1,6 +1,6 @@
 //
 //  ========================================================================
-//  Copyright (c) 1995-2016 Webtide LLC, Olivier Lamy
+//  Copyright (c) 1995-2017 Mort Bay Consulting Pty. Ltd.
 //  ------------------------------------------------------------------------
 //  All rights reserved. This program and the accompanying materials
 //  are made available under the terms of the Eclipse Public License v1.0
@@ -14,6 +14,7 @@
 //
 //  You may elect to redistribute this code under either of these licenses.
 //  ========================================================================
+//
 
 package org.mortbay.jetty.load.generator.store;
 
