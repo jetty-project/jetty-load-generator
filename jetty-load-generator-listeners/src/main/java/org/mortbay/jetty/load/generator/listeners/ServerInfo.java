e0188cba7cbf66a0140d59e04f1bc320ea68d723
olivier lamy <oliver.lamy@gmail.com>
2018-11-07 10:17:34 +1000

    client already started here
    Signed-off-by: olivier lamy <oliver.lamy@gmail.com>

--- jetty-load-generator-listeners/src/main/java/org/mortbay/jetty/load/generator/listeners/ServerInfo.java
+++ jetty-load-generator-listeners/src/main/java/org/mortbay/jetty/load/generator/listeners/ServerInfo.java
@@ -97,7 +97,6 @@ public class ServerInfo
     public static ServerInfo retrieveServerInfo( Request request, HttpClient httpClient )
         throws Exception
     {
-            httpClient.start();
             ContentResponse contentResponse = httpClient //
                 .newRequest( request.host, request.port ) //
                 .scheme( request.scheme ) //
